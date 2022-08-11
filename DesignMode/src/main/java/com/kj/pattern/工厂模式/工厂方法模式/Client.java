561034c849ccbfef529d571a8cc237d9f867bef9
kuangjie <1760141843@qq.com>
2022-08-11 14:29:08 +0800

    工厂模式

---
+++ DesignMode/src/main/java/com/kj/pattern/工厂模式/工厂方法模式/Client.java
@@ -0,0 +1,8 @@
+package com.kj.pattern.工厂模式.工厂方法模式;
+
+/**
+ * @Author: kj
+ * @Date: 2022/08/11/14:28
+ */
+public class Client {
+}
---
+++ DesignMode/src/main/java/com/kj/pattern/工厂模式/抽象工厂模式/Client.java
@@ -0,0 +1,8 @@
+package com.kj.pattern.工厂模式.抽象工厂模式;
+
+/**
+ * @Author: kj
+ * @Date: 2022/08/11/14:28
+ */
+public class Client {
+}
