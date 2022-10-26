af9c99ce65e45c1aab6aebda82c9b15a4fd6d4aa
kuangjie <1760141843@qq.com>
2022-10-26 16:37:15 +0800

    springcloud

---
+++ springCloud_study/springcloud_eureka_zuul/src/main/java/com/example/spirngcloud_eureka_zuul/SpirngcloudEurekaZuulApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngcloud_eureka_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngcloudEurekaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngcloudEurekaZuulApplication.class, args);
+	}
+
+}
---
+++ springCloud_study/springcloud_eureka_zuul/src/test/java/com/example/spirngcloud_eureka_zuul/SpirngcloudEurekaZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngcloud_eureka_zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngcloudEurekaZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
