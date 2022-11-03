a9a7c2582e9551903b571ba378eae042ffe2f3ad
kuangjie <1760141843@qq.com>
2022-11-03 18:05:50 +0800

    test1.txt

---
+++ kafka/kafka-springboot/src/main/java/com/example/kafkaspringboot/aop/LogAspect.java
@@ -0,0 +1,5 @@
+package com.example.kafkaspringboot.aop;/**
+ * @Author: kj
+ * @Date: 2022/11/02/14:26
+ */public class LogAspect {
+}
---
+++ kafka/kafka-springboot/src/main/java/com/example/kafkaspringboot/pojo/ErrorLog.java
@@ -0,0 +1,5 @@
+package com.example.kafkaspringboot.pojo;/**
+ * @Author: kj
+ * @Date: 2022/11/02/15:11
+ */public class ErrorLog {
+}
